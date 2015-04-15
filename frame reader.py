611960fdfc50bdef1656e0d0e6330a3b5feffe29
ragarg <trall_99@mail.ru>
2015-04-15 13:35:27 +0400

    Update frame reader.py

--- frame reader.py
+++ frame reader.py
@@ -1,16 +1,32 @@
+from geometry import *
+from random import randint 
+
+ABC = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+ABC_2 = ['α', 'β', 'γ', 'δ', 'ε', 'ζ', 'η', 'θ', 'ι', 'κ', 'λ', 'μ', 'ν', 'ξ', 'ο', 'π', 'ρ', 'σ', 'τ', 'υ', 'φ', 'χ', 'ψ']
+
 def enter():
     print("How many points?")
-    points = int(input())
-    information = data(points)
+    number = int(input())
+    information = data(number)
+    print("Your points:", end=' ')
+    print(" ".join(information))
     information_2 = reader(information)
     printer()
-    
 
-def data(points):
-    pass
+def data(number):
+    points = []
+    for i in range(number):
+        points.append(str(ABC[i]) + ' ' + str(randint()) + ' ' + str(randint()))
+    return points    
 
 def reader():
-    pass
-
+    '''
+    segments
+    mid
+    triangle
+    triangle information
+    cercle
+    beem
+    '''
 def printer():
     pass
