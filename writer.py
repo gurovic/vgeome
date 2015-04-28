10b6dc64d3e913d4cf03941d6a95b04a27336f8c
KimSergey <kims230599@gmail.com>
2015-04-28 18:57:27 +0300

    Delete writer.py

--- writer.py
+++
@@ -1,6 +0,0 @@
-def write(objects):
-    writer = open('output', 'w')
-    for obj in objects:
-        print(type(obj), file=writer)
-        print(obj, file=writer)
-        print('---', file=writer)
