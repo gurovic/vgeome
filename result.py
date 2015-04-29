37412caf2e20e0aea69843f5b897db19b485ecbd
KimSergey <kims230599@gmail.com>
2015-04-29 13:44:21 +0400

    Update result.py

--- result.py
+++ result.py
@@ -1,19 +1,28 @@
+import random
 import geometry
-import reader
+import generator
 
-points = reader.points
-segments = reader.segments
+points = generator.points
+segments = generator.segments
 
 for point1 in points:
     for point2 in points:
         for point3 in points:
-            if point1 != point2 and point1 != point3 and point2 != point3:
+            if point1[0] != point2[0] and point1[0] != point3[0] and point2[0] != point3[0]:
                 if point1[0] in geometry.Line(point2[0], point3[0]):
                     print(point1[1] + ' ' + point2[1] + ' ' + point3[1] + ' on one line')
 
-for segment1 in segments:
-    for segment2 in segments:
+for point1 in points:
+    for point2 in points:
+        for point3 in points:
+            for point4 in points:
+                if point1[0] in geometry.Circle(point2[0], point3[0], point4[0]):
+                    print(point1[1] + ' ' + point2[1] + ' ' + point3[1] + ' ' + point4[1] + ' on one circle')
+
+for segment1 in generator.segments:
+    for segment2 in generator.segments:
         if (geometry.Line(segment1[0].A, segment1[0].B) & geometry.Line(segment2[0].A, segment2[0].B)) == ():
             print(segment1[1] + ' parallel ' + segment2[1])
         if segment1[0] == segment2[0]:
-            print(segment1[1] + ' = ' + segment2[1])
+            if segment1 != segment2:
+                print(segment1[1] + ' = ' + segment2[1])
