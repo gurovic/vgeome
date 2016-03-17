66aead741999a74e036f1246a5b391cae4ea475a
KimSergey <kims230599@gmail.com>
2016-03-17 13:47:21 +0400

    Update geometry.java

--- geometry.java
+++ geometry.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collections;
+
 
 class Point {
     public double x, y;
@@ -12,20 +11,24 @@ class Point {
         y = second;
     }
 
-    public String str() {
-        return ("Coordiants: " + Double.toString(x) + ", " + Double.toString(y));
+    public String toString() {
+        return "Coordinates: " + Double.toString(x) + ", " + Double.toString(y);
     }
 
     public Point neg() {
-        Point otherPoint = new Point(-x, -y);
-        return (otherPoint);
+        Point newPoint = new Point(-x, -y);
+        return newPoint;
+    }
+
+    public Point add(Vector otherVector) {
+        Point newPoint = new Point(x + otherVector.x, y + otherVector.y);
+        return newPoint;
     }
 }
 
 
 class Vector {
     public double x, y;
-    public Point A, B;
 
     Vector(double first, double second) {
         x = first;
@@ -33,18 +36,99 @@ class Vector {
     }
 
     Vector(Point first, Point second) {
-        A = first;
-        B = second;
-        x = B.x - A.x;
-        y = B.y - A.y;
+        x = second.x - first.x;
+        y = second.y - first.y;
     }
 
-    public String str() {
-        return ("Coordiants: " + Double.toString(x) + ", " + Double.toString(y));
+    public String toString() {
+        return "Coordinates: " + Double.toString(x) + ", " + Double.toString(y);
     }
 
     public double abs() {
-        return (Math.pow(x * x + y * y, 0.5));
+        return Math.pow(x * x + y * y, 0.5);
+    }
+
+    public Vector mul(double otherNumber) {
+        Vector newVector = new Vector(x * otherNumber, y * otherNumber);
+        return newVector;
+    }
+
+    public Vector add(Vector otherVector) {
+        Vector newVector = new Vector(x + otherVector.x, y + otherVector.y);
+        return newVector;
+    }
+
+    public Vector div(double otherNumber) {
+        Vector newVector = new Vector(x / otherNumber, y / otherNumber);
+        return newVector;
+    }
+    public Vector sub(Vector otherVector) {
+        Vector newVector = new Vector(x - otherVector.x, y - otherVector.y);
+        return newVector;
+    }
+
+    public double productVector(Vector otherVector) {
+        return x * otherVector.y - y * otherVector.x;
+    }
+
+    public double productScalar(Vector otherVector) {
+        return x * otherVector.x + y * otherVector.y;
+    }
+}
+
+
+class Line {
+    public double A, B, C;
+
+    Line(double first, double second, double third) {
+        A = first;
+        B = second;
+        C = third;
+    }
+
+    Line(Point first, Point second) {
+        A = first.y - second.y;
+        B = second.x - first.x;
+        C = first.x * second.y - first.y * second.x;
+    }
+
+    public double dist(Point otherPoint) {
+        return (A * otherPoint.x + B * otherPoint.y + C) / Math.pow((A * A + B * B), 0.5);
+    }
+
+    public Point[] cross(Line otherLine) {
+        if (A * otherLine.B != B * otherLine.A) {
+           Point crossPoint = new Point((B * otherLine.C - C * otherLine.B) / (A * otherLine.B - B * otherLine.A),
+                                        (C * otherLine.A - A * otherLine.C) / (A * otherLine.B - B * otherLine.A));
+           Point[] crossPoints = {crossPoint};
+        return crossPoints;
+    }
+}
+
+
+class Circle {
+    public Point O;
+    public double radius;
+
+    Circle(Point first, double second) {
+        O = first;
+        radius = second;
+    }
+}
+
+
+class Triangle {
+    public Point A, B, C;
+    public Vector AB, BA, AC, CA, BC, CB;
+
+    Triangle(Point first, Point second, Point third) {
+        A = first;
+        B = second;
+        C = third;
+    }
+
+    public double orientedArea() {
+        return AB.cross(AC) / 2;
     }
 }
 
@@ -56,11 +140,13 @@ public class Hello {
         double number2 = Double.parseDouble(in.readLine());
         double number3 = Double.parseDouble(in.readLine());
         double number4 = Double.parseDouble(in.readLine());
+        double number5 = Double.parseDouble(in.readLine());
         Point myp1 = new Point(number1, number2);
         Point myp2 = new Point(number3, number4);
         Vector myv1 = new Vector(myp1, myp2);
-        System.out.println(myp1.str() + "; " + myp2.str());
-        System.out.println(myv1.str());
-        System.out.println(myv1.abs());
+        System.out.println("first point: " + myp1.toString() + "; " + "second point: " + myp2.toString());
+        System.out.println("vector " + myv1.toString());
+        System.out.println("vector length: " + myv1.abs());
+        System.out.println("new coordinats of first points: " + myp1.add(myv1.mul(number5)).toString());
     }
 }
